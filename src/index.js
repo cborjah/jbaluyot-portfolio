53ea2d97fa31c6ee8166764b7e6d3093d7811753
cborjah <cpborja32@gmail.com>
2018-09-20 14:28:10 -0700

    sets up redux

--- src/index.js
+++ src/index.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from "react-redux";
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import store from './store';
 
 import 'normalize.css';
 import './style/index.css';
+import Navbar from './components/Navbar';
 
 import Home from './components/Home';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Switch>
-      <Navbar />
-      <Route path="/" component={Home} />
-    </Switch>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Switch>
+        <Navbar />
+        <Route path="/" component={Home} />
+      </Switch>
+    </BrowserRouter>
+  </Provider>
   , document.getElementById('root'));
 registerServiceWorker();
